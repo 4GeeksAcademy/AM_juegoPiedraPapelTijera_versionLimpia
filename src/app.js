85661167848f9a40c1b398eefe43522b3317e15d
Antonino Morana <amorana1508@gmail.com>
2025-05-11 23:54:35 +0000

    Limpieza codigo, Proyecto Terminado

--- src/app.js
+++ src/app.js
@@ -1,103 +1,43 @@
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/4geeks.ico";
-import { Button } from "bootstrap";
 
-// const button = document.getElementsByClassName('myButton');
+const weaponOptions = [
+  { name: "rock", enemyList: ["paper", "spock"] },
+  { name: "paper", enemyList: ["scissors", "lizard"] },
+  { name: "scissors", enemyList: ["rock", "spock"] },
+  { name: "lizard", enemyList: ["scissors", "rock"] },
+  { name: "spock", enemyList: ["paper", "lizard"] },
+];
 
-  // const _getIsListInclude = ( gameArray , reference ) => {
-  //   gameArray.reduce( (accumulator, element)=> { 
-  //     if (reference == element){ accumulator = true; return accumulator };
-  //     accumulator = false ; return accumulator; 
-  //   }
-  //    , false)
-  //   };
-  
-const _randomize = (anArray) => {
-  const randomNumber = Math.floor(Math.random()*anArray.length);
-    const wordInstance = anArray[randomNumber];
-  return wordInstance;
-};
-
-const rock = {name : 'rock', enemyList : ['paper', 'spock']};
-const paper = {name : 'paper', enemyList : ['scissors', 'lizard']}; 
-const scissors = {name : 'scissors', enemyList : ['rock', 'spock']}; 
-const lizard = {name : 'lizard', enemyList : ['scissors', 'rock']}; 
-const spock = {name : 'spock', enemyList : ['paper', 'lizard']}; 
+const _randomize = (array) => array[Math.floor(Math.random() * array.length)];
 
 const userSelects = (id) => {
-  //Podemos mirar refactor
-  let selectedId = '';
-    if(id == rock.name) {
-      console.log('user chose rock');
-      document.getElementById('user-selection').innerText = 'rock';
-      selectedId = 'rock';
-      return rock; 
-    }
-    if(id == paper.name) {
-      console.log('user chose paper');
-      document.getElementById('user-selection').innerText = 'paper';
-      selectedId = 'paper';
-      return paper;
-    }
-    if(id == scissors.name) {
-      console.log('user chose scissors');
-      document.getElementById('user-selection').innerText = 'scissors';
-      selectedId = 'scissors';
-      return scissors;
-    }
-    if(id == lizard.name) {
-      console.log('user chose lizard');
-      document.getElementById('user-selection').innerText = 'lizard';
-      selectedId = 'lizard';
-      return lizard;
-    }
-    if(id == spock.name) {
-      console.log('user chose spock');
-      document.getElementById('user-selection').innerText = 'spock';
-      selectedId = 'spock';
-      return spock;
-    };
-}; 
-  
-function playGame(userSelection){
-  
-  const possibleSelections = [ rock , paper , scissors , lizard , spock];
-  const pcSelector = () =>  _randomize(possibleSelections);
-  const pcSelection = pcSelector().name;
+  return weaponOptions.find((weapon) => weapon.name === id);
+};
 
-  let isInEnemyList = function(userSelects){
-    let enemyList =  userSelects.enemyList;
-      for(let enemy of enemyList){
-        if(pcSelection === enemy) {return true}; 
-        
-      }
-      return false; 
-  } ; 
-  
-    document.getElementById('pc-selection').innerText = `${pcSelection}`;
-    if(pcSelection === userSelection) {
-      console.log('draw');
-      return document.getElementById('result').innerText = 'DRAW';
-      
+const isInEnemyList = (userSelection, pcSelection) => 
+  userSelection.enemyList.includes(pcSelection);
+
+function playGame(userSelection) {
+  const pcSelection = _randomize(weaponOptions).name;
+  document.getElementById("pc-selection").innerText = pcSelection;
+
+  if (pcSelection === userSelection.name) {
+    document.getElementById("result").innerText = "DRAW";
+  } else if (isInEnemyList(userSelection, pcSelection)) {
+    document.getElementById("result").innerText = "PC WINS! You lose.";
+  } else {
+    document.getElementById("result").innerText = "USER WINS!";
+  }
+}
+
+weaponOptions.forEach((weapon) => {
+  document.getElementById(weapon.name).addEventListener("click", () => {
+    const userSelection = userSelects(weapon.name);
+    if (userSelection) {
+      document.getElementById("user-selection").innerText = userSelection.name;
+      playGame(userSelection);
     }
-
-    const userSelectionEnemyList = userSelection.enemyList;
-    console.log(userSelection.enemyList);
-
-    if(isInEnemyList(userSelection)){
-        return document.getElementById('result').innerText = 'PC WINS you lose ';
-    }
-    {
-        return document.getElementById('result').innerText = 'USER WINS';
-        }
-
-    }
-        
-  document.getElementById('rock').addEventListener('click', () => {playGame( userSelects('rock'))});
-  document.getElementById('paper').addEventListener('click', () => {playGame( userSelects('paper'))});
-  document.getElementById('scissors').addEventListener('click', () => {playGame( userSelects('scissors'))});
-  document.getElementById('lizard').addEventListener('click', () => {playGame(userSelects('lizard'))});
-  document.getElementById('spock').addEventListener('click', () => {playGame(userSelects('spock'))});
-
+  });
+});
